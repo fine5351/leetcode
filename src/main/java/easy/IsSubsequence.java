0446e2f98bb21f1a8e50f7e56966214c15ac2673
erickuo <eric0970569800@gmail.com>
2021-10-16 08:48:32 +0800

    20211016
    紀錄

--- src/main/java/easy/IsSubsequence.java
+++ src/main/java/easy/IsSubsequence.java
@@ -2,15 +2,17 @@ package easy;
 
 import utils.PrintUtils;
 
+import java.util.*;
+
 public class IsSubsequence {
 
     public static void main(String[] args) {
         IsSubsequence isSubsequence = new IsSubsequence();
-        System.out.println(isSubsequence.isSubsequence("abc", "ahbgdc"));
-        System.out.println(isSubsequence.isSubsequence("axc", "ahbgdc"));
-        System.out.println(isSubsequence.isSubsequence("b", "c"));
-        System.out.println(isSubsequence.isSubsequence("acb", "ahbgdc"));
-        System.out.println(isSubsequence.isSubsequence("bb", "ahbgdc"));
+//        System.out.println(isSubsequence.isSubsequence("abc", "ahbgdc"));
+//        System.out.println(isSubsequence.isSubsequence("axc", "ahbgdc"));
+//        System.out.println(isSubsequence.isSubsequence("b", "c"));
+//        System.out.println(isSubsequence.isSubsequence("acb", "ahbgdc"));
+//        System.out.println(isSubsequence.isSubsequence("bb", "ahbgdc"));
         System.out.println(isSubsequence.isSubsequence("b", "abc"));
     }
 
@@ -21,17 +23,45 @@ public class IsSubsequence {
         if (t.isEmpty()) {
             return false;
         }
-        int i = 0;
-        int j = 0;
         char[] chars = s.toCharArray();
         char[] chart = t.toCharArray();
-        while (i < chars.length && j < chart.length) {
-            if (chars[i] == chart[j]) {
-                i++;
-            }
-            j++;
+        Map<Character, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < chart.length; i++) {
+            List<Integer> appearIndexList = map.getOrDefault(chart[i], new ArrayList<>());
+            appearIndexList.add(i);
+            map.put(chart[i], appearIndexList);
         }
-        return i == chars.length;
+
+        int lastIndex = -1;
+        for (int i = 0; i < chars.length; i++) {
+            List<Integer> appearIndexList = map.get(chars[i]);
+            if (appearIndexList == null) {
+                return false;
+            } else {
+                int findIndex = findFirstAppearIndex(lastIndex, appearIndexList);
+                lastIndex = findIndex;
+                System.out.println("find char " + chars[i] + ", lastIndex " + i + ", first index " + findIndex);
+                if (findIndex == -1) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private int findFirstAppearIndex(int lastIndex, List<Integer> list) {
+        int left = 0;
+        int right = list.size();
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int middle = list.get(mid);
+            if (middle > lastIndex) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left == list.size() ? -1 : list.get(left);
     }
 
 //    public boolean isSubsequence(String s, String t) {
