4aec6c7252e3b27ce0647dfefceac3bca47024ac
erickuo <eric0970569800@gmail.com>
2021-10-17 19:59:13 +0800

    20211017
    紀錄

---
+++ src/main/java/medium/RepeatedDNASequence.java
@@ -0,0 +1,37 @@
+package medium;
+
+import easy.AddDigits;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import utils.PrintUtils;
+
+import javax.swing.text.AbstractDocument;
+import java.util.*;
+
+public class RepeatedDNASequence {
+
+    private static final Logger logger = LoggerFactory.getLogger(RepeatedDNASequence.class);
+
+    public static void main(String[] args) {
+        RepeatedDNASequence repeatedDNASequence = new RepeatedDNASequence();
+//        PrintUtils.printList(repeatedDNASequence.findRepeatedDnaSequences("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"));
+        PrintUtils.printList(repeatedDNASequence.findRepeatedDnaSequences("AAAAAAAAAAA"));
+    }
+
+    public List<String> findRepeatedDnaSequences(String s) {
+        List<String> result = new ArrayList<>();
+        if (s.length() > 10) {
+            Set<String> hasAppearSet = new HashSet<>();
+            for (int i = 0; i <= s.length() - 10; i++) {
+                String slideWindow = s.substring(i, i + 10);
+                logger.info("slideWindow : {}", slideWindow);
+                if (hasAppearSet.contains(slideWindow) && !result.contains(slideWindow)) {
+                    result.add(slideWindow);
+                } else {
+                    hasAppearSet.add(slideWindow);
+                }
+            }
+        }
+        return result;
+    }
+}
