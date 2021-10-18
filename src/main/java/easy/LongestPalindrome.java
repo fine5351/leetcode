ed907495bf1fc2e907e4b0f6e8dba84e96756935
erickuo <eric0970569800@gmail.com>
2021-10-18 22:09:27 +0800

    20211018
    紀錄

--- src/main/java/easy/LongestPalindrome.java
+++ src/main/java/easy/LongestPalindrome.java
@@ -1,11 +1,5 @@
 package easy;
 
-import java.awt.image.RescaleOp;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class LongestPalindrome {
 
     public static void main(String[] args) {
@@ -18,22 +12,22 @@ public class LongestPalindrome {
 
     public int longestPalindrome(String s) {
         char[] chars = s.toCharArray();
-        int[] indexs = new int[52];
-        for (int i = 0; i < chars.length; i++) {
-            int index = chars[i] - 97;
+        int[] allTypeChars = new int[52];
+        for (char c : chars) {
+            int index = c - 97;
             if (index >= 0) {
-                indexs[index]++;
+                allTypeChars[index]++;
             } else {
-                indexs[index + 32 + 26]++;
+                allTypeChars[index + 32 + 26]++;
             }
         }
         int length = 0;
         int oddCount = 0;
-        for (int i = 0; i < indexs.length; i++) {
-            if (indexs[i] % 2 != 0) {
+        for (int charCount : allTypeChars) {
+            if (charCount % 2 != 0) {
                 oddCount++;
             }
-            length += indexs[i];
+            length += charCount;
         }
         return length - (oddCount > 0 ? oddCount - 1 : 0);
     }
