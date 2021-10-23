3672ff334b1e6276b24f87d09ba8cd53eb5dbee3
erickuo <eric0970569800@gmail.com>
2021-10-23 15:33:20 +0800

    20211023
    紀錄

---
+++ src/main/java/medium/FindDuplicateSubtrees.java
@@ -0,0 +1,37 @@
+package medium;
+
+import utils.TreeNode;
+
+import java.util.*;
+
+public class FindDuplicateSubtrees {
+
+    public static void main(String[] args) {
+        FindDuplicateSubtrees findDuplicateSubtrees = new FindDuplicateSubtrees();
+        findDuplicateSubtrees.findDuplicateSubtrees(new TreeNode(1, new TreeNode(2, new TreeNode(4), null), new TreeNode(3,
+                new TreeNode(2, new TreeNode(4), null), new TreeNode(4))));
+    }
+
+    private Map<String, Integer> subTreeMap = new HashMap<>();
+    private List<TreeNode> result = new LinkedList<>();
+
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        travel(root, "");
+        return result;
+    }
+
+    private String travel(TreeNode root, String path) {
+        if (root == null) {
+            return "#";
+        }
+        String leftPath = travel(root.left, path + root.val);
+        String rightPath = travel(root.right, path + root.val);
+        String subTree = leftPath + "," + rightPath + "," + root.val;
+        int appearCount = subTreeMap.getOrDefault(subTree, 0) + 1;
+        if (appearCount == 2) {
+            result.add(root);
+        }
+        subTreeMap.put(subTree, appearCount);
+        return subTree;
+    }
+}
---
+++ src/main/java/medium/MaximumBinaryTree.java
@@ -0,0 +1,67 @@
+package medium;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import utils.PrintUtils;
+import utils.TreeNode;
+
+import java.util.concurrent.TimeUnit;
+
+public class MaximumBinaryTree {
+
+    private static final Logger logger = LoggerFactory.getLogger(MaximumBinaryTree.class);
+
+    public static void main(String[] args) {
+        MaximumBinaryTree maximumBinaryTree = new MaximumBinaryTree();
+        TreeNode root = maximumBinaryTree.constructMaximumBinaryTree(new int[]{3, 2, 1, 6, 0, 5});
+        PrintUtils.printTreePreorder(root);
+    }
+
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        return construct(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode construct(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+        int maxIndex = -1;
+        int maxValue = -1;
+        for (int i = left; i <= right; i++) {
+            if (maxValue < nums[i]) {
+                maxIndex = i;
+                maxValue = nums[i];
+            }
+        }
+        TreeNode root = new TreeNode(maxValue);
+        root.left = construct(nums, left, maxIndex - 1);
+        root.right = construct(nums, maxIndex + 1, right);
+        return root;
+    }
+//    public TreeNode constructMaximumBinaryTree(int[] nums) {
+//        return construct(nums, 0, nums.length, new int[nums.length]);
+//    }
+//
+//    private TreeNode construct(int[] nums, int left, int right, int[] isUsed) {
+//        if (left >= right) {
+//            return null;
+//        }
+//        int maxIndex = -1;
+//        int maxValue = -1;
+//        for (int i = left; i < right; i++) {
+//            if (maxValue < nums[i]) {
+//                maxIndex = i;
+//                maxValue = nums[i];
+//            }
+//        }
+//        if (isUsed[maxIndex] == 0) {
+//            TreeNode root = new TreeNode(maxValue);
+//            isUsed[maxIndex] = 1;
+//            root.left = construct(nums, left, maxIndex, isUsed);
+//            root.right = construct(nums, maxIndex + 1, right, isUsed);
+//            return root;
+//        } else {
+//            return null;
+//        }
+//    }
+}
